41e375d2b93cb992db4511385f8741eff329b85e
Omar Salaheldine <OmarSalaheldine@Omars-MacBook-Pro.local>
2020-05-04 03:37:46 -0700

    logger

--- utils/logger.py
+++ utils/logger.py
@@ -9,8 +9,11 @@ class Logger(object):
 
     def scalar_summary(self, tag, value, step):
         """Log a scalar variable."""
-        summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value)])
-        self.writer.add_summary(summary, step)
+        with self.writer.as_default():
+            tf.summary.scalar(tag, value, step=step)
+            self.writer.flush()
+        #summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value)])
+        #self.writer.add_summary(summary, step)
 
     def list_of_scalars_summary(self, tag_value_pairs, step):
         """Log scalar variables."""
