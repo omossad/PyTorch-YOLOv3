4cfc5eb364435b9a8f34a7a51d8ace60a882f61f
Omar Salaheldine <OmarSalaheldine@Omars-MacBook-Pro.local>
2020-05-04 04:39:57 -0700

    custom

--- models.py
+++ models.py
@@ -142,7 +142,7 @@ class YOLOLayer(nn.Module):
         self.img_dim = img_dim
         num_samples = x.size(0)
         grid_size = x.size(2)
-        print(grid_size)
+        print(x.shape)
 
         prediction = (
             x.view(num_samples, self.num_anchors, self.num_classes + 5, grid_size, grid_size)
