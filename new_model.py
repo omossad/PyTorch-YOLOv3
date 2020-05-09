f21c090ed504add11d3476b68e0e1dfc732cf803
Omar Salaheldine <OmarSalaheldine@Omars-MacBook-Pro.local>
2020-05-08 23:39:31 -0700

    change

--- new_model.py
+++ new_model.py
@@ -402,7 +402,7 @@ class Darknet(nn.Module):
                 yolo_outputs.append(x)
             elif module_def["type"] == "roi":
                 yolo_outputs = to_cpu(torch.cat(yolo_outputs, 1))
-                roi_x, roi_y, roi_loss = self.roi_layer(yolo_outputs)
+                roi_x, roi_y, roi_loss = module[0](yolo_outputs)
                 loss = roi_loss
                 print('ROI LOSS')
                 print(roi_loss)
