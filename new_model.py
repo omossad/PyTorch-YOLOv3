17ca8e90fd31408cfcd3eab48eb625970ab60c2f
Omar Salaheldine <OmarSalaheldine@Omars-MacBook-Pro.local>
2020-05-08 18:34:50 -0700

    print

--- new_model.py
+++ new_model.py
@@ -293,7 +293,7 @@ class ROILayer(nn.Module):
                 y_tile = y_tiles.data.tolist()[i]
                 s_obj  = obj_class.data.tolist()[i]
                 s_conf = obj_conf.data.tolist()[i]
-                print(str(x_tiles.data.tolist()[i]) + ' ' + str(x_tiles.data.tolist()[i]))
+                print(str(x_coordinate.data.tolist()[i]) + ' ' + str(x_coordinate.data.tolist()[i]))
                 print(str(image_i) + ' ' + str(x_tile) + ' ' + str(y_tile) + ' ' + str(s_obj) + ' ' + str(s_conf) + '\n')
                 x_inpt[image_i][x_tile][s_obj] += s_conf
                 x_inpt[image_i][y_tile][s_obj] += s_conf
