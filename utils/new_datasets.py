cb406f4854de31e84b47f6efa2578ddb91d1f3af
Omar Salaheldine <OmarSalaheldine@Omars-MacBook-Pro.local>
2020-05-09 01:45:20 -0700

    targets

--- utils/new_datasets.py
+++ utils/new_datasets.py
@@ -106,27 +106,28 @@ class ListDataset(Dataset):
         if os.path.exists(label_path):
             print('READ')
             print(np.loadtxt(label_path))
-            boxes = torch.from_numpy(np.loadtxt(label_path).reshape(-1, 5))
-            print('RESHAPE')
-            print(boxes)
+            #boxes = torch.from_numpy(np.loadtxt(label_path).reshape(-1, 5))
+            targets = torch.from_numpy(np.loadtxt(label_path).reshape(-1, 2))
+            #print('RESHAPE')
+            #print(labels)
             # Extract coordinates for unpadded + unscaled image
-            x1 = w_factor * (boxes[:, 1] - boxes[:, 3] / 2)
-            y1 = h_factor * (boxes[:, 2] - boxes[:, 4] / 2)
-            x2 = w_factor * (boxes[:, 1] + boxes[:, 3] / 2)
-            y2 = h_factor * (boxes[:, 2] + boxes[:, 4] / 2)
+            #x1 = w_factor * (boxes[:, 1] - boxes[:, 3] / 2)
+            #y1 = h_factor * (boxes[:, 2] - boxes[:, 4] / 2)
+            #x2 = w_factor * (boxes[:, 1] + boxes[:, 3] / 2)
+            #y2 = h_factor * (boxes[:, 2] + boxes[:, 4] / 2)
             # Adjust for added padding
-            x1 += pad[0]
-            y1 += pad[2]
-            x2 += pad[1]
-            y2 += pad[3]
+            #x1 += pad[0]
+            #y1 += pad[2]
+            #x2 += pad[1]
+            #y2 += pad[3]
             # Returns (x, y, w, h)
-            boxes[:, 1] = ((x1 + x2) / 2) / padded_w
-            boxes[:, 2] = ((y1 + y2) / 2) / padded_h
-            boxes[:, 3] *= w_factor / padded_w
-            boxes[:, 4] *= h_factor / padded_h
+            #boxes[:, 1] = ((x1 + x2) / 2) / padded_w
+            #boxes[:, 2] = ((y1 + y2) / 2) / padded_h
+            #boxes[:, 3] *= w_factor / padded_w
+            #boxes[:, 4] *= h_factor / padded_h
 
-            targets = torch.zeros((len(boxes), 6))
-            targets[:, 1:] = boxes
+            #targets = torch.zeros((len(boxes), 6))
+            #targets[:, 1:] = boxes
             print('FINAL')
             print(targets.shape)
             print(targets)
