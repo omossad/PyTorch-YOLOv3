b3f85b6a2eab36065711a84bcd4a70bbc1760059
Omar Salaheldine <OmarSalaheldine@Omars-MacBook-Pro.local>
2020-05-11 09:44:10 -0700

    floor

--- utils/roi_utils.py
+++ utils/roi_utils.py
@@ -306,7 +306,7 @@ def build_targets(pred_boxes, pred_cls, target, anchors, ignore_thres):
     b, target_labels = target[:, :2].long().t()
     gx, gy = gxy.t()
     gw, gh = gwh.t()
-    gi, gj = gxy.long().t()
+    gi, gj = gxy.floor().long().t()
     print('PROBLEM')
     print(gi)
     print(gj)
