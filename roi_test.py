5baadfd514be85b86ab63442ba10720d3e71bf1e
Omar Salaheldine <OmarSalaheldine@Omars-MacBook-Pro.local>
2020-05-11 03:51:24 -0700

    test

--- roi_test.py
+++ roi_test.py
@@ -23,6 +23,7 @@ import torch.optim as optim
 def evaluate(model, path, iou_thres, conf_thres, nms_thres, img_size, batch_size):
     model.eval()
 
+
     # Get dataloader
     dataset = ListDataset(path, img_size=img_size, augment=False, multiscale=False)
     dataloader = torch.utils.data.DataLoader(
