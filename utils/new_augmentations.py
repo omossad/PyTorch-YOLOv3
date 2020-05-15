68fc403a5182f62d9fc5a7be1de8dc3f2cc95d79
Omar Salaheldine <OmarSalaheldine@Omars-MacBook-Pro.local>
2020-05-14 23:12:06 -0700

    tiles

--- utils/new_augmentations.py
+++ utils/new_augmentations.py
@@ -3,7 +3,7 @@ import torch.nn.functional as F
 import numpy as np
 
 
-def horizontal_flip(images, targets, num_tiles=8):
+def horizontal_flip(images, targets, num_tiles=4):
     images = torch.flip(images, [-1])
     targets[:, 1] = (num_tiles-1) - targets[:, 1]
     return images, targets
