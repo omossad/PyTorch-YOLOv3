fd75361af1b2ba46de99000610cb8f2a746ce3bf
Omar Salaheldine <OmarSalaheldine@Omars-MacBook-Pro.local>
2020-05-22 05:54:42 -0700

    cuda

--- new_model.py
+++ new_model.py
@@ -14,6 +14,7 @@ import matplotlib.patches as patches
 
 import warnings
 warnings.filterwarnings("ignore", category=UserWarning)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def create_modules(module_defs):
     """
