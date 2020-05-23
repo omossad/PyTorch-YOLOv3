c0ae7d1d07955fc851e342d7e2fd47b780fe36ff
Omar Salaheldine <OmarSalaheldine@Omars-MacBook-Pro.local>
2020-05-23 07:48:07 -0700

    add roi layers

--- roi_model.py
+++ roi_model.py
@@ -304,7 +304,7 @@ class ROI(nn.Module):
         self.metrics = {}
         #self.module_defs = parse_model_config(config_path)
         #self.hyperparams, self.module_list = create_modules(self.module_defs)
-        #self.yolo_layers = [layer[0] for layer in self.module_list if hasattr(layer[0], "metrics")]
+        self.roi_layers = [layer[0] for layer in self.module_list if hasattr(layer[0], "metrics")]
         self.img_size = img_size
         self.loss_func = nn.CrossEntropyLoss()
         self.fc_out_x = nn.Sequential(
