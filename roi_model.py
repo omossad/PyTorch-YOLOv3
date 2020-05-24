0385a4407e7f068a6c5a23ea91a0c04c5c58fd89
Omar Salaheldine <OmarSalaheldine@Omars-MacBook-Pro.local>
2020-05-24 10:14:57 -0700

    tiles

--- roi_model.py
+++ roi_model.py
@@ -377,7 +377,7 @@ class ROI(nn.Module):
         self.img_size = img_size
         self.loss_func = nn.CrossEntropyLoss()
         self.fc_out = nn.Sequential(
-            nn.Linear(self.num_tiles * self.num_tiles, 64),
+            nn.Linear(self.num_tiles, 64),
             #nn.Linear(self.num_tiles * self.num_classes, 512),
             nn.LeakyReLU(inplace=False),
             nn.Dropout(),
