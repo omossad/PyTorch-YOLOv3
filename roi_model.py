ac74169d1f75bf995543fd26690aceac70c7e6bb
Omar Salaheldine <OmarSalaheldine@Omars-MacBook-Pro.local>
2020-05-24 09:06:37 -0700

    more complex model

--- roi_model.py
+++ roi_model.py
@@ -378,16 +378,16 @@ class ROI(nn.Module):
         self.loss_func = nn.CrossEntropyLoss()
         self.fc_out = nn.Sequential(
             #nn.Linear(self.num_classes * self.num_tiles, 64),
-            nn.Linear(self.num_tiles, 10),
+            nn.Linear(self.num_tiles, 64),
             nn.LeakyReLU(inplace=False),
-            #nn.Dropout(),
-            #nn.Linear(64, 64),
-            #nn.LeakyReLU(inplace=False),
-            #nn.Dropout(),
+            nn.Dropout(),
+            nn.Linear(64, 64),
+            nn.LeakyReLU(inplace=False),
+            nn.Dropout(),
             #nn.BatchNorm1d(64),
-            nn.Linear(10, 10),
+            nn.Linear(64, 32),
             nn.LeakyReLU(inplace=False),
-            nn.Linear(10, self.num_tiles)
+            nn.Linear(32, self.num_tiles)
             #nn.Sigmoid(inplace=True)
         )
 
