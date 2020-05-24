9ba3ff660bec755ab1b2dab304d793102f86da19
Omar Salaheldine <OmarSalaheldine@Omars-MacBook-Pro.local>
2020-05-24 05:50:15 -0700

    augmentation

--- utils/roi_augmentations.py
+++ utils/roi_augmentations.py
@@ -3,7 +3,7 @@ import torch.nn.functional as F
 import numpy as np
 
 
-def horizontal_flip(images, targets, num_htiles=5):
+def horizontal_flip(images, targets, num_htiles=4):
     images = torch.flip(images, [-1])
     targets[:, 1] = (num_htiles-1) - targets[:, 1]
     return images, targets
