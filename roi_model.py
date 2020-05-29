87e0c22f1a2c1cab4204d4cd1eff874a8e47735e
Omar Salaheldine <OmarSalaheldine@Omars-MacBook-Pro.local>
2020-05-29 02:06:57 -0700

    long instead of float

--- roi_model.py
+++ roi_model.py
@@ -459,8 +459,8 @@ class Encoder(nn.Module):
         FloatTensor = torch.cuda.FloatTensor
         LongTensor = torch.cuda.LongTensor
         ByteTensor = torch.cuda.ByteTensor
-        return (torch.zeros(self.num_layers, batch_size, self.hidden_dim).type(FloatTensor),
-                torch.zeros(self.num_layers, batch_size, self.hidden_dim).type(FloatTensor))
+        return (torch.zeros(self.num_layers, batch_size, self.hidden_dim).type(LongTensor),
+                torch.zeros(self.num_layers, batch_size, self.hidden_dim).type(LongTensor))
 
     def forward(self, inputs):
         # Push through RNN layer (the ouput is irrelevant)
