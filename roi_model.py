afc968e821b652f4c2760dff3460363d974d7c78
Omar Salaheldine <OmarSalaheldine@Omars-MacBook-Pro.local>
2020-05-29 02:04:23 -0700

    fix

--- roi_model.py
+++ roi_model.py
@@ -475,7 +475,7 @@ class Decoder(nn.Module):
         self.lstm = nn.LSTM(1, hidden_dim, num_layers=num_layers).cuda()
         self.out = nn.Linear(hidden_dim, 1)
         self.loss_func = nn.CrossEntropyLoss()
-        self.num_tiles = 4
+        self.num_tiles = 16
 
     def forward(self, outputs, hidden):
         # Tensors for cuda support
