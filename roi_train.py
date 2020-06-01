45ac479c9507bbd55db1a4ae02805331dea1f490
Omar Salaheldine <OmarSalaheldine@Omars-MacBook-Pro.local>
2020-06-01 12:15:50 -0700

    print

--- roi_train.py
+++ roi_train.py
@@ -126,6 +126,8 @@ if __name__ == "__main__":
             #x_inpt = yolo_preprocessing(yolo_outputs, opt.htiles, 0, opt.classes, opt.img_size)
             #y_inpt = yolo_preprocessing(yolo_outputs, opt.vtiles, 1, opt.classes, opt.img_size)
             x_inpt = yolo_single_tile(yolo_outputs, opt.htiles, opt.classes, opt.img_size)
+            print('x inpt')
+            print(x_inpt)
             #print(x_inpt)
             #x_inpt = Variable(x_inpt.to(device))
             #print(x_inpt.shape)
