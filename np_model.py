43a3c90db89d16992d48dca28338a087cff71fdc
Omar Salaheldine <OmarSalaheldine@Omars-MacBook-Pro.local>
2020-06-04 16:02:15 -0700

    data1

--- np_model.py
+++ np_model.py
@@ -3,7 +3,7 @@ import pprint, pickle
 data_path = '/home/omossad/scratch/temp/numpy/'
 pkl_file = open(data_path + 'data_array.pkl', 'rb')
 
-data = pickle.load(pkl_file)
+data1 = pickle.load(pkl_file)
 pprint.pprint(data1)
 
 pkl_file.close()
