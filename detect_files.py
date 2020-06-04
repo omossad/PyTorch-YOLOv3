de6f34e10cafc9362171823cdaa456c30a6dbfa0
Omar Salaheldine <OmarSalaheldine@Omars-MacBook-Pro.local>
2020-06-04 01:21:27 -0700

    config

--- detect_files.py
+++ detect_files.py
@@ -24,7 +24,7 @@ from matplotlib.ticker import NullLocator
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--image_folder", type=str, default="data/samples", help="path to dataset")
-    parser.add_argument("--model_def", type=str, default="config/new_custom_tiny.cfg", help="path to model definition file")
+    parser.add_argument("--model_def", type=str, default="config/base_model.cfg", help="path to model definition file")
     parser.add_argument("--weights_path", type=str, default="checkpoints/tiny_yolo.pth", help="path to weights file")
     parser.add_argument("--class_path", type=str, default="data/custom/annotations/classes.names", help="path to class label file")
     parser.add_argument("--conf_thres", type=float, default=0.0, help="object confidence threshold")
