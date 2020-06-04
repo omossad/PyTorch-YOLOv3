e25cd13389899e4a07c3539b10fa389dfeedb930
Omar Salaheldine <OmarSalaheldine@Omars-MacBook-Pro.local>
2020-06-04 16:19:53 -0700

    targets

--- np_model.py
+++ np_model.py
@@ -1,9 +1,11 @@
 import pprint, pickle
+import numpy as np
 
 data_path = '/home/omossad/scratch/temp/numpy/'
 pkl_file = open(data_path + 'data_array.pkl', 'rb')
 
-data1 = pickle.load(pkl_file)
+data = pickle.load(pkl_file)
+targets = np.loadtxt(data_path + 'trgt_array.dat')
 #pprint.pprint(data1)
-print(data1)
+print(targets)
 pkl_file.close()
