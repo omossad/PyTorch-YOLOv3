d6fc2fe78c73c6184fe0c362d7258aa7edb1adac
Omar Salaheldine <OmarSalaheldine@Omars-MacBook-Pro.local>
2020-06-04 21:08:51 -0700

    print

--- np_model.py
+++ np_model.py
@@ -24,14 +24,15 @@ classes_no = 7
 
 model = nn.LSTM(in_size, classes_no, 2)
 input_seq = Variable(torch.randn(time_steps, batch_size, in_size))
-print(input_seq)
+print(input_seq.shape)
 output_seq, _ = model(input_seq)
-print(output_seq)
+print(output_seq,shape)
 
 last_output = output_seq[-1]
-
+print(last_output.shape)
 loss = nn.CrossEntropyLoss()
 target = Variable(torch.LongTensor(batch_size).random_(0, classes_no-1))
+print(target.shape)
 err = loss(last_output, target)
 err.backward()
 
