debaed777832d4da94edac35d53ecc6728b0c11e
Omar Salaheldine <OmarSalaheldine@Omars-MacBook-Pro.local>
2020-06-05 18:25:31 -0700

    8x8

--- detect_files.py
+++ detect_files.py
@@ -100,7 +100,7 @@ if __name__ == "__main__":
     #colors = [cmap(i) for i in np.linspace(0, 1, 20)]
 
     print("\nSaving images:")
-    txy_labels_path = '/home/omossad/scratch/temp/roi/labels4x4/'
+    txy_labels_path = '/home/omossad/scratch/temp/roi/labels8x8/'
     t_labels_path = '/home/omossad/scratch/temp/roi/labels/'
     out_path = '/home/omossad/scratch/temp/numpy/'
     # Iterate through images and save plot of detections
