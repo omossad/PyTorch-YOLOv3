766ba25d3a79a25bb6e381e274d88c656c39d9cd
Omar Salaheldine <OmarSalaheldine@Omars-MacBook-Pro.local>
2020-06-12 04:07:08 -0700

    add new changes

--- preprocessing/csv_fixation_to_frame_fixation.py
+++ preprocessing/csv_fixation_to_frame_fixation.py
@@ -9,26 +9,47 @@ import math
 ### THIS FILE GENERATE TILE DATA     ###
 ### FROM RAW FRAMES AND FIXATION     ###
 ########################################
+'/home/omossad/scratch/Gaming-Dataset/processed/frame_labels/fifa'
+## VARIABLES ###
+# input folder is where the fixation points are located #
+input_folder = '/home/omossad/scratch/Gaming-Dataset/labels/fifa/'
+# output folder is where the frame fixations will be stored#
+output_folder = '/home/omossad/scratch/Gaming-Dataset/processed/raw_frame_labels/fifa/'
 
-num_files = 10
+
 base_dir = '/home/omossad/scratch/temp/fixations/'
 out_dir = '/home/omossad/scratch/temp/labels/'
 
-#def find_nearest(array, value):
-#	array = np.asarray(array)
-#	idx = (np.abs(array - value)).argmin()
-#	return array[idx]
+
+
 
 def find_nearest(array,value):
+	'''
+	function to find the nearest fixation point to the current frame
+	takes as input an array of fixations and the frame index (time offset in this case)
+	'''
 	idx = np.searchsorted(array, value, side="left")
 	if idx > 0 and (idx == len(array) or math.fabs(value - array[idx-1]) < math.fabs(value - array[idx])):
 		return idx-1
 	else:
 		return idx
+#def find_nearest(array, value):
+#	array = np.asarray(array)
+#	idx = (np.abs(array - value)).argmin()
+#	return array[idx]
+
+num_files = 0
+with open('frames_info', 'r') as f:
+    for line in f:
+        num_files += 1
+print("Total number of lines is:", count)
+# number of files is the number of files to be processed #
+num_files = num_files - 1
 
 
 frame_time = np.zeros((num_files,1))
 frame_info = np.zeros((num_files,2))
+file_names = []
 with open('frames_info') as csv_file:
 	csv_reader = csv.reader(csv_file, delimiter=',')
 	line_count = 0
@@ -36,6 +57,7 @@ with open('frames_info') as csv_file:
 		if line_count == 0:
 			line_count += 1
 		elif line_count < num_files+1:
+			filenames.append(row[0])
 			frame_time[line_count-1] = int(row[5])
 			frame_info[line_count-1] = [int(row[2]), int(row[3])]
 			line_count += 1
@@ -45,7 +67,7 @@ with open('frames_info') as csv_file:
 for i in range(num_files):
 	fixations_time = []
 	fixations_val  = []
-	with open(base_dir + 'User '+str(i)+'_fixations.csv') as csv_file:
+	with open(input_folder + file_names[i]+ '.csv') as csv_file:
 		csv_reader = csv.reader(csv_file, delimiter=',')
 		line_count = 0
 		for row in csv_reader:
@@ -56,10 +78,10 @@ for i in range(num_files):
 				fixations_val.append([float(row[2]), float(row[3])])
 				line_count += 1
 		print(f'Real Fixations are {line_count}')
-	
+
 	labels = []
 	frames = 0
-	start_frame = int(frame_info[i][0]) 
+	start_frame = int(frame_info[i][0])
 	end_frame = int(frame_info[i][1])
 #	start_frame = 0
 #	end_frame = 734
@@ -70,5 +92,5 @@ for i in range(num_files):
 		idx = find_nearest(fixations_time, current_frame_time)
 #		print(idx)
 #		print(fixations_val[idx])
-		labels.append(fixations_val[idx])			
-	np.savetxt(out_dir+'labels_'+str(i)+'.txt', labels)
+		labels.append(fixations_val[idx])
+	np.savetxt(output_folder+'labels_'+file_names[i]+'.txt', labels)
--- preprocessing/distinct_obj.py
+++ preprocessing/distinct_obj.py
@@ -1,6 +1,11 @@
+
 import numpy as np
 import csv
-print(float(0.355))
+
+'''
+This code is to print the number of distinct objects/tiles/labels in a file
+
+'''
 #data = np.loadtxt('/home/omossad/projects/def-hefeeda/omossad/roi_detection/data/fifa/1/fixation1.txt')
 #data = np.loadtxt('/home/omossad/scratch/Gaming-Dataset/processed/puria/fifa/0000-scrn.avi/label.txt')
 file = '/home/omossad/scratch/Gaming-Dataset/processed/puria/fifa/0007-scrn.avi/label.txt'
