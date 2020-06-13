0eb128fb53bbd56b711be05f2b909b2aee71cd54
Omar Salaheldine <OmarSalaheldine@Omars-MacBook-Pro.local>
2020-06-13 00:39:11 -0700

    trace

--- detect_files.py
+++ detect_files.py
@@ -32,7 +32,7 @@ if __name__ == "__main__":
     parser.add_argument("--weights_path", type=str, default="checkpoints/tiny_yolo.pth", help="path to weights file")
     parser.add_argument("--class_path", type=str, default="data/custom/annotations/classes.names", help="path to class label file")
     parser.add_argument("--conf_thres", type=float, default=0.0, help="object confidence threshold")
-    parser.add_argument("--nms_thres", type=float, default=0.0, help="iou thresshold for non-maximum suppression")
+    parser.add_argument("--nms_thres", type=float, default=0.0, help="iou threshold for non-maximum suppression")
     parser.add_argument("--batch_size", type=int, default=1, help="size of the batches")
     parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
     parser.add_argument("--img_size", type=int, default=416, help="size of each image dimension")
@@ -108,9 +108,7 @@ if __name__ == "__main__":
 
         # Save image and detections
         imgs.extend(img_paths)
-        print(img_paths)
-        print(opt.image_folder.split("/")[-1])
-        filename = img_paths.split("/")[-1].split(".")[0]
+        filename = img_paths[0].split("/")[-1].split(".")[0]
         filename = detections_folder + filename + '.dat'
 
         np.savetxt(filename, np.asarray(detections))
