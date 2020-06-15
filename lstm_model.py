abb0476eb339ea27f6aec1097b99a1fd2e72b1f3
Omar Salaheldine <OmarSalaheldine@Omars-MacBook-Pro.local>
2020-06-15 15:28:17 -0700

    trace

--- lstm_model.py
+++ lstm_model.py
@@ -48,7 +48,7 @@ def process_data(data):
     batch_size = 4
     num_images = len(data)
     image_indices = np.arange(0,num_images)
-    indices = np.array([ image_indices[i:i+time_steps] for i in range(len(image_indices)-time_steps+1) ])
+    indices = np.array([ image_indices[i:i+time_steps] for i in range(num_images-time_steps) ])
     data = np.asarray(data)
     data = np.reshape(data, (num_images,-1))
     img_data = []
