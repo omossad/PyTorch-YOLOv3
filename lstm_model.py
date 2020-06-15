4d4654a52e72152ca155c9683626069b4b032bfa
Omar Salaheldine <OmarSalaheldine@Omars-MacBook-Pro.local>
2020-06-15 02:25:33 -0700

    fix

--- lstm_model.py
+++ lstm_model.py
@@ -14,14 +14,7 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 def closestNumber(n, m) :
     q = int(n / m)
-    n1 = m * q
-    if((n * m) > 0) :
-        n2 = (m * (q + 1))
-    else :
-        n2 = (m * (q - 1))
-    if (abs(n - n1) < abs(n - n2)) :
-        return n1
-    return n2
+    return int(q*m)
 
 
 def read_info():
