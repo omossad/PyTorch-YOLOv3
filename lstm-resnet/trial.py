d7e5cdbbc9d6faab5d189c236e3fbce9e1d114ce
Omar Salaheldine <OmarSalaheldine@Omars-MacBook-Pro.local>
2020-06-19 01:55:48 -0700

    fix

--- lstm-resnet/trial.py
+++ lstm-resnet/trial.py
@@ -1,4 +1,5 @@
 import glob
+import numpy as np
 ha_0_images = sorted(glob.glob("/home/omossad/scratch/temp/roi/images/ha_0_*"))
 ha_0_labels = sorted(glob.glob("/home/omossad/scratch/temp/roi/labels/ha_0_*"))
 print(len(ha_0_images))
