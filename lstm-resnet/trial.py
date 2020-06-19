7b0cd8fff4d9dcad839beb2acdbdac547923baf2
Omar Salaheldine <OmarSalaheldine@Omars-MacBook-Pro.local>
2020-06-19 01:44:58 -0700

    trial

--- lstm-resnet/trial.py
+++ lstm-resnet/trial.py
@@ -1,3 +1,5 @@
 import glob
-a = glob.glob("/home/omossad/scratch/temp/roi/*")
-print(a[0])
+ha_0_images = glob.glob("/home/omossad/scratch/temp/roi/images/ha_0_*")
+ha_0_labels = glob.glob("/home/omossad/scratch/temp/roi/labels/ha_0_*")
+print(ha_0_images[0])
+print(ha_0_labels[0])
