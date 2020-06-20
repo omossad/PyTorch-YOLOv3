2e4c1a35f1ee850e20b7a0d7c3400ca2feeee67d
Omar Salaheldine <OmarSalaheldine@Omars-MacBook-Pro.local>
2020-06-19 22:44:06 -0700

    try now

--- lstm-pretrained/functions.py
+++ lstm-pretrained/functions.py
@@ -120,7 +120,7 @@ class ResCNNEncoder(nn.Module):
         self.fc_hidden1, self.fc_hidden2 = fc_hidden1, fc_hidden2
         self.drop_p = drop_p
 
-        resnet = models.mobilenet_v2(pretrained=True)
+        resnet = models.resnet152(pretrained=True)
         modules = list(resnet.children())[:-1]      # delete the last fc layer.
         self.resnet = nn.Sequential(*modules)
         self.fc1 = nn.Linear(resnet.fc.in_features, fc_hidden1)
---
+++ lstm-pretrained/trial.py
@@ -0,0 +1,39 @@
+import glob
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+ha_0_images = sorted(glob.glob("/home/omossad/scratch/temp/roi/images/ha_0_*"))
+ha_0_labels = sorted(glob.glob("/home/omossad/scratch/temp/roi/labels/ha_0_*"))
+print(len(ha_0_images))
+print(len(ha_0_labels))
+
+
+time_steps = 4
+
+
+def process_data(images):
+    num_images = len(images)
+    image_indices = np.arange(0,num_images)
+    indices = np.array([ image_indices[i:i+time_steps] for i in range(num_images-time_steps) ])
+    images=np.asarray(images)
+    print(images[indices].shape)
+    return images[indices]
+
+def process_labels(labels):
+    num_labels = len(labels)
+    indices = np.arange(time_steps-1,num_labels-1)
+    labels=np.asarray(labels)
+    print(labels[indices].shape)
+    return labels[indices]
+
+
+a = process_data(ha_0_images)
+b = process_labels(ha_0_labels)
+train_list, test_list, train_label, test_label = train_test_split(a, b, test_size=0.25, random_state=42)
+
+print(len(train_list))
+print(len(test_list))
+print(train_list[10])
+print(test_list[5])
+print(train_label[10])
+print(test_label[5])
