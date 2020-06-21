9dda48ad572856d9c11ce35c344b5af6e738d518
Omar Salaheldine <OmarSalaheldine@Omars-MacBook-Pro.local>
2020-06-20 22:54:18 -0700

    try resent

--- lstm-mobilenet/functions.py
+++ lstm-mobilenet/functions.py
@@ -120,12 +120,10 @@ class ResCNNEncoder(nn.Module):
         self.fc_hidden1, self.fc_hidden2 = fc_hidden1, fc_hidden2
         self.drop_p = drop_p
 
-        resnet = models.mobilenet_v2(pretrained=True)
-        #modules = list(resnet.children())[:-1]      # delete the last fc layer.
-        modules = list(resnet.children())
+        resnet = models.resnet18(pretrained=True)
+        modules = list(resnet.children())[:-1]      # delete the last fc layer.
         self.resnet = nn.Sequential(*modules)
-        self.fc1 = nn.Linear(1000, fc_hidden1)
-        #self.fc1 = nn.Linear(resnet.fc.in_features, fc_hidden1)
+        self.fc1 = nn.Linear(resnet.fc.in_features, fc_hidden1)
         self.bn1 = nn.BatchNorm1d(fc_hidden1, momentum=0.01)
         self.fc2 = nn.Linear(fc_hidden1, fc_hidden2)
         self.bn2 = nn.BatchNorm1d(fc_hidden2, momentum=0.01)
