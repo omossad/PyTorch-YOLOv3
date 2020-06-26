a8f43e957bd62d8708df3942c665c3b910e358cc
Omar Salaheldine <OmarSalaheldine@Omars-MacBook-Pro.local>
2020-06-26 00:04:33 -0700

    try again

--- lstm-pretrained/mobilenet.py
+++ lstm-pretrained/mobilenet.py
@@ -62,7 +62,7 @@ class ResNet(nn.Module):
         """Load the pretrained ResNet-152 and replace top fc layer."""
         super(ResNet, self).__init__()
         resnet = models.mobilenet_v2(pretrained=True)
-        summary(resnet, (3, 224, 224))
+        summary(resnet, (244, 224, 3))
         #print(resnet)
         modules = list(resnet.children())[:-1]      # delete the last fc layer.
         self.resnet = nn.Sequential(*modules)
