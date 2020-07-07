4bf77b8b5901ca234d65515b6a633c364288e64c
Omar Salaheldine <OmarSalaheldine@Omars-MacBook-Pro.local>
2020-07-06 17:09:30 -0700

    try params

--- lstm-pretrained/main.py
+++ lstm-pretrained/main.py
@@ -17,16 +17,16 @@ import pickle
 import csv
 
 # EncoderCNN architecture
-CNN_fc_hidden1, CNN_fc_hidden2 = 1024, 768
-CNN_embed_dim = 256   # latent dim extracted by 2D CNN
+CNN_fc_hidden1, CNN_fc_hidden2 = 1024, 256
+CNN_embed_dim = 128   # latent dim extracted by 2D CNN
 
 res_size = 224        # ResNet image size
-dropout_p = 0.0       # dropout probability
+dropout_p = 0.5       # dropout probability
 
 # DecoderRNN architecture
-RNN_hidden_layers = 3
-RNN_hidden_nodes = 512
-RNN_FC_dim = 256
+RNN_hidden_layers = 2
+RNN_hidden_nodes = 128
+RNN_FC_dim = 128
 
 # training parameters
 k = 8            # number of target category
