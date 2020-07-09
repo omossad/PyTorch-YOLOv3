f92d2f2616810e647c7bb0b714223f979c4b5a74
Omar Salaheldine <OmarSalaheldine@Omars-MacBook-Pro.local>
2020-07-09 02:49:32 -0700

    100 epochs

--- keras_code/main.py
+++ keras_code/main.py
@@ -119,7 +119,7 @@ model = keras.Sequential([
 model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
-model.fit(train_images, train_labels, epochs=10)
+model.fit(train_images, train_labels, epochs=100)
 test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
 
 print('\nTest accuracy:', test_acc)
