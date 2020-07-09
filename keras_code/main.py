8433d0472a25f5f1c215f54b1e4df0cbb6b0d967
Omar Salaheldine <OmarSalaheldine@Omars-MacBook-Pro.local>
2020-07-09 00:43:47 -0700

    import csv

--- keras_code/main.py
+++ keras_code/main.py
@@ -6,6 +6,7 @@ import torch
 #from tensorflow import keras
 
 import numpy as np
+import csv
 #data = torch.load('/home/omossad/scratch/Gaming-Dataset/features/fifa/yolov3-tiny/ha_0/frame_00251.pt',map_location=lambda storage, loc: storage)
 #print(data)
 W = 1920
