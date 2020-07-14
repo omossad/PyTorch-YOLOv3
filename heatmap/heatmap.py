527b7f05168aedcfd60c5daef029bd4f5296c5a7
Omar Salaheldine <OmarSalaheldine@Omars-MacBook-Pro.local>
2020-07-14 12:04:51 -0700

    heatmap

---
+++ heatmap/heatmap.py
@@ -0,0 +1,78 @@
+'''
+mport numpy as np
+import numpy.random
+import matplotlib.pyplot as plt
+
+# Generate some test data
+
+x = np.random.randn(8873)
+y = np.random.randn(8873)
+
+heatmap, xedges, yedges = np.histogram2d(x, y, bins=50)
+extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
+
+plt.clf()
+plt.imshow(heatmap.T, extent=extent, origin='lower')
+plt.show()
+
+from matplotlib import pyplot as PLT
+from matplotlib import cm as CM
+from matplotlib import mlab as ML
+import numpy as NP
+
+n = 1e5
+x = y = NP.linspace(-5, 5, 100)
+X, Y = NP.meshgrid(x, y)
+Z1 = ML.bivariate_normal(X, Y, 2, 2, 0, 0)
+Z2 = ML.bivariate_normal(X, Y, 4, 1, 1, 1)
+ZD = Z2 - Z1
+x = X.ravel()
+y = Y.ravel()
+z = ZD.ravel()
+gridsize=30
+PLT.subplot(111)
+
+# if 'bins=None', then color of each hexagon corresponds directly to its count
+# 'C' is optional--it maps values to x-y coordinates; if 'C' is None (default) then
+# the result is a pure 2D histogram
+
+PLT.hexbin(x, y, C=z, gridsize=gridsize, cmap=CM.jet, bins=None)
+PLT.axis([x.min(), x.max(), y.min(), y.max()])
+
+cb = PLT.colorbar()
+cb.set_label('mean value')
+PLT.show()
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+from scipy.ndimage.filters import gaussian_filter
+
+
+def myplot(x, y, s, bins=1000):
+    heatmap, xedges, yedges = np.histogram2d(x, y, bins=bins)
+    heatmap = gaussian_filter(heatmap, sigma=s)
+
+    extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
+    return heatmap.T, extent
+
+
+fig, axs = plt.subplots(2, 2)
+
+# Generate some test data
+x = np.random.randn(1000)
+y = np.random.randn(1000)
+
+sigmas = [0, 16, 32, 64]
+
+for ax, s in zip(axs.flatten(), sigmas):
+    if s == 0:
+        ax.plot(x, y, 'k.', markersize=5)
+        ax.set_title("Scatter plot")
+    else:
+        img, extent = myplot(x, y, s)
+        ax.imshow(img, extent=extent, origin='lower', cmap=cm.jet)
+        ax.set_title("Smoothing with  $\sigma$ = %d" % s)
+
+plt.show()
