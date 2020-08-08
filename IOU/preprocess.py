eb489e22197ecec7b461b3d0728030485fe6bf76
Omar Salaheldine <OmarSalaheldine@Omars-MacBook-Pro.local>
2020-08-07 17:17:05 -0700

    try now

--- IOU/preprocess.py
+++ IOU/preprocess.py
@@ -108,7 +108,12 @@ for i in range(num_files):
         sx_tile, sy_tile = get_tileXY(lbl_x,lbl_y)
         box_1 = get_box_r(sx_tile, sy_tile)
         box_2 = get_box_c(lbl_x, lbl_y, 70)
-		
+		iou_avg = iou_avg + iou(box_1, box_2)
+        counter =  counter + 1
+	iou_avg = iou_avg / counter
+	print(iou_avg)
+iou_avg = iou_avg / num_files
+print(iou_avg)
 		#print(lbl_x)
         #print(lbl_y)
 		#print(sx_tile)
